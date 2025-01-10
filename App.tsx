27a45497a1ecee2be85e66830aae15db83593f76
Pedro <pedrohrb77@gmail.com>
2025-01-10 11:41:33 -0300

    chore: create tests with actions

--- App.tsx
+++ App.tsx
@@ -1,11 +1,26 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function App() {
+  const [counter, setCounter] = useState(0);
   return (
     <View style={styles.container}>
-      <Text>Auto Tests React Native</Text>
+      <Text style={styles.mainTitle}>Auto Tests React Native</Text>
       <StatusBar style="auto" />
+
+      <Text style={styles.title}>App Contador</Text>
+      <View style={styles.counterContainer}>
+        <TouchableOpacity onPress={() => setCounter(counter - 1)}>
+          <Text>-</Text>
+        </TouchableOpacity>
+
+        <Text testID="counter">{counter}</Text>
+
+        <TouchableOpacity onPress={() => setCounter(counter + 1)}>
+          <Text>+</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -16,5 +31,19 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  mainTitle: {
+    fontSize: 22,
+    fontWeight: "bold",
+  },
+  title: {
+    fontSize: 16,
+    marginTop: 12,
+    fontWeight: "semibold",
+  },
+  counterContainer: {
+    flexDirection: "row",
+    gap: 14,
+    marginVertical: 14,
   },
 });
--- __test__/app/App.test.tsx
+++ __test__/app/App.test.tsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react-native";
+import { fireEvent, render } from "@testing-library/react-native";
 
 import App from "../../App";
 
@@ -7,5 +7,27 @@ describe("App Component", () => {
     const { getByText } = render(<App />);
 
     expect(getByText("Auto Tests React Native")).toBeTruthy();
+  });
+
+  it("should increase counter onPress plus button", () => {
+    const { getByText, getByTestId } = render(<App />);
+
+    const plusButton = getByText("+");
+    fireEvent.press(plusButton);
+
+    const counterText = getByTestId("counter");
+
+    expect(counterText.props.children).toBe(1);
+  });
+
+  it("should decrease counter onPress minus button", () => {
+    const { getByText, getByTestId } = render(<App />);
+
+    const plusButton = getByText("-");
+    fireEvent.press(plusButton);
+
+    const counterText = getByTestId("counter");
+
+    expect(counterText.props.children).toBe(-1);
   });
 });
